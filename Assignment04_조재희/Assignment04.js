210bad4bf9c48f1b9c6976adbee3cf5bdcee4630
jaehee <jaehee3679@daum.net>
2022-05-18 19:21:27 +0900

    finish

---
+++ Assignment04_조재희/Assignment04.js
@@ -0,0 +1,36 @@
+let shape=["H","S","D","C"];
+let cardcase=[];
+let realcardcase=[];
+
+
+for (let i=0;i<shape.length;i++){
+    for(let j=1;j<=13;j++){
+        if(j==1){
+            cardcase.push(shape[i]+"A");
+        }else if(j==10){
+            cardcase.push(shape[i]+"T");
+        }else if(j==11){
+            cardcase.push(shape[i]+"J");
+        }else if(j==12){
+            cardcase.push(shape[i]+"Q");
+         }else if(j==13){
+            cardcase.push(shape[i]+"K");
+         }else{
+            cardcase.push(shape[i]+j);
+         }
+    }
+}
+
+console.log(Math.random()*52+1);
+const cardset=new Set();
+const number=52;
+console.log(cardset.size);
+while(cardset.size<number){
+    const randomNumber=parseInt(Math.random()*52);
+    cardset.add(randomNumber);
+}
+let arr = Array.from(cardset);
+for(let i=0;i<arr.length;i++){
+    realcardcase.push(cardcase[arr[i]]);
+}
+console.log(realcardcase);
